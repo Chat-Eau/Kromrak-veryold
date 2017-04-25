c9e16c52dbf6740b5d5386f506c1a9d127a4744b
lapb290796 <benjaminlaprise@yahoo.ca>
2017-04-25 14:36:56 -0400

    Commit test

---
+++ src/Application.java
@@ -0,0 +1,12 @@
+import Evenements.Combat;
+
+/**
+ * Created by lapb290796 on 2017-03-28.
+ */
+public class Application {
+    public static void main(String[] args) {
+        Combat combatTest = new Combat();
+//test commit
+        combatTest.combat();
+    }
+}
